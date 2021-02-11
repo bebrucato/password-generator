5cb848b8c4974c3f2c2594a257a63317a83a154d
B Brucato <bebrucato@gmail.com>
2021-02-10 16:06:12 -0800

    setting characters

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function generatePassword () {
   console.log("New Password");
 }
 
- var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-console.log(values);
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -17,7 +19,7 @@ function writePassword() {
 
 }
 
-for(var i=0; i <= password; i++)
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
